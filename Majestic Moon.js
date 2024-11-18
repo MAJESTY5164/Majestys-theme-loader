28578f3435101d9f17817587c6b85fbc8e998e18
MAJESTY5164 <majesty5164@gmail.com>
2024-11-18 12:07:33 -0600

    Create Majestic Moon.js

---
+++ Majestic Moon.js
@@ -0,0 +1,10 @@
+document.querySelector("html").className = "custom-theme-background full-motion theme-dark platform-web font-size-16";
+const style = document.createElement("style");
+style.setAttribute("data-client-themes", "true");
+style.setAttribute("data-rh", "true");
+style.textContent = `
+  .custom-theme-background {
+      --custom-theme-background: linear-gradient(64.92deg, #FF00B6 0%, #000000 100%);
+  }
+`;
+document.head.appendChild(style);
