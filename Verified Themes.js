9421bcfbfba29589b73bdc313e49ecd6e83af5e2
MAJESTY5164 <majesty5164@gmail.com>
2024-11-19 09:31:50 -0600

    Update Verified Themes.js

--- Verified Themes.js
+++ Verified Themes.js
@@ -1,5 +1,7 @@
     window.VerifiedThemes = function(x) {
         if (x === "Majestic Moon") {
             CustomTheme("https://raw.githubusercontent.com/MAJESTY5164/Majestys-theme-loader/refs/heads/main/Majestic%20Moon.js");
+        }else if (x === "Whiteout") {
+            CustomTheme("https://raw.githubusercontent.com/MAJESTY5164/Majestys-theme-loader/refs/heads/main/Whiteout.js");
         }
     }
