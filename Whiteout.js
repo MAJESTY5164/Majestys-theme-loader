d89b21de1de86afccf0ba9cbe10cb7e7ac3404d4
MAJESTY5164 <majesty5164@gmail.com>
2024-11-19 17:19:00 -0600

    Update Whiteout.js

--- Whiteout.js
+++ Whiteout.js
@@ -1,10 +1,19 @@
-document.querySelector("html").className = "custom-theme-background full-motion theme-dark platform-web font-size-16";
-const style = document.createElement("style");
-style.setAttribute("data-client-themes", "true");
-style.setAttribute("data-rh", "true");
-style.textContent = `
-  .custom-theme-background {
-      --custom-theme-background: linear-gradient(64.92deg, #000000 0%, #ffffff 100%);
-  }
-`;
-document.head.appendChild(style);
+ //Whiteout
+ (function() {
+    window.names.push("Whiteout");
+    window.icon.push("linear-gradient(64.92deg, #000000 0%, #ffffff 100%)");
+    window.funcname.push("Whiteout");
+
+    window.Whiteout = function() {
+        document.querySelector("html").className = "custom-theme-background full-motion theme-dark platform-web font-size-16";
+        const style = document.createElement("style");
+        style.setAttribute("data-client-themes", "true");
+        style.setAttribute("data-rh", "true");
+        style.textContent = `
+        .custom-theme-background {
+            --custom-theme-background: linear-gradient(64.92deg, #000000 0%, #ffffff 100%);
+        }
+        `;
+        document.head.appendChild(style);
+    };
+})();
