d5bb4b61249a4d3ff453153e2305be8170561ffc
MAJESTY5164 <majesty5164@gmail.com>
2024-11-19 18:35:20 -0600

    Update Verified Themes.js

--- Verified Themes.js
+++ Verified Themes.js
@@ -1,7 +1,3 @@
-    window.VerifiedThemes = function(x) {
-        if (x === "Majestic Moon") {
-            CustomTheme("https://raw.githubusercontent.com/MAJESTY5164/Majestys-theme-loader/refs/heads/main/Majestic%20Moon.js");
-        }else if (x === "Whiteout") {
-            CustomTheme("https://raw.githubusercontent.com/MAJESTY5164/Majestys-theme-loader/refs/heads/main/Whiteout.js");
-        }
-    }
+https://raw.githubusercontent.com/MAJESTY5164/Majestys-theme-loader/refs/heads/main/Whiteout.js
+https://raw.githubusercontent.com/MAJESTY5164/Majestys-theme-loader/refs/heads/main/Rainbow.js
+https://raw.githubusercontent.com/MAJESTY5164/Majestys-theme-loader/refs/heads/main/Majestic%20Moon.js
